86fffbd10531b9652bd1fe8df434326f3aa6b719
mbuenafe <Mark@The-Markbook.local>
2017-03-29 14:10:38 -0700

    PLUGIN

--- plugin/redditgold/content.js
+++ plugin/redditgold/content.js
@@ -1,59 +1,37 @@
 
 $('body').children().each(function () {
 	// replace the '@' sign with a '$' sign
+		// $(this).html( $(this).html().replace(/\$14\./g,'RG3.72') );
+		// $(this).html( $(this).html().replace(/\$15\.88/g,'RG3.98') );
+		// $(.currency-symbol).html( $(.currency-symbol).html().replace(/\$/g,'RG') );
 
-	$(this).html( $(this).html().replace(/\$11\.14/g,'RG3') );
-	$(this).html( $(this).html().replace(/\$11\.18/g,'RG3') );
-	$(this).html( $(this).html().replace(/\$11\.34/g,'RG3') );
-	$(this).html( $(this).html().replace(/\$15\.95/g,'RG4') );
-	$(this).html( $(this).html().replace(/\$189\.00/g,'RG48') );
-	$(this).html( $(this).html().replace(/\$12\.95/g,'RG3') );
-	$(this).html( $(this).html().replace(/\$6\.49/g,'RG2') );
-	$(this).html( $(this).html().replace(/\$16\.00/g,'RG4') );
-	$(this).html( $(this).html().replace(/\$133\.04/g,'RG33') );
-	$(this).html( $(this).html().replace(/\$17\.59/g,'RG5') );
-	$(this).html( $(this).html().replace(/\$9\.22/g,'RG2') );
-	$(this).html( $(this).html().replace(/\$37\.95/g,'RG3') );
-	$(this).html( $(this).html().replace(/\$7\.54/g,'RG2') );
-	$(this).html( $(this).html().replace(/\$3\.75/g,'RG1') );
-	$(this).html( $(this).html().replace(/\$12\.50/g,'RG3') );
-	$(this).html( $(this).html().replace(/\$19\.99/g,'RG5') );
-	$(this).html( $(this).html().replace(/\$24\.59/g,'RG6S') );
-	$(this).html( $(this).html().replace(/\$9\.99/g,'RG3') );
-	$(this).html( $(this).html().replace(/\$33\.79/g,'RG9') );
-	$(this).html( $(this).html().replace(/\$25\.99/g,'RG7') );
-	$(this).html( $(this).html().replace(/\$118\.99/g,'RG30') );
-	$(this).html( $(this).html().replace(/\$39\.99/g,'RG10') );
-
-});
-
-
-
-
-
-//
-// // replace text in the page when it loads
-// $('body').children().each(function () {
-//
-// // replace the '@' sign with a '$' sign
-// $(this).html( $(this).html().replace(/$/g,'WOW!') );
-// });
-//
-
-// // replace images when the page loads
-// $('img').attr('src', 'http://i.dailymail.co.uk/i/pix/2015/09/15/13/2C554FAA00000578-3235219-image-m-70_1442319933551.jpg');
-//
-
-// // when you click on an image hide it using CSS
-// $('img').click(function() {
-// 	$(this).addClass('hide');
-// });
-
-
-
-// // when you click on the page 'body' apply .gradient to it from the CSS
-// $('body').click(function() {
-// 	$(this).addClass('gradient');
-// });
-//
-// $('#hplogo').addClass('rotate');
+	$(this).html( $(this).html().replace(/\$20\.45/g,'₹₲5') );
+	$(this).html( $(this).html().replace(/\$18\.99/g,'₹₲5') );
+	$(this).html( $(this).html().replace(/\$23\.56/g,'₹₲6') );
+	$(this).html( $(this).html().replace(/\$14\.55/g,'₹₲4') );
+	$(this).html( $(this).html().replace(/\$35\.00/g,'₹₲9') );
+	$(this).html( $(this).html().replace(/\$14\.20/g,'₹₲4') );
+	$(this).html( $(this).html().replace(/\$16\.32/g,'₹₲4') );
+	$(this).html( $(this).html().replace(/\$43\.95/g,'₹₲11') );
+	$(this).html( $(this).html().replace(/\$19\.03/g,'₹₲7') );
+	$(this).html( $(this).html().replace(/\$18\.24/g,'₹₲5') );
+	$(this).html( $(this).html().replace(/\$37\.21/g,'₹₲9') );
+	$(this).html( $(this).html().replace(/\$9\.99/g,'₹₲3') );
+	$(this).html( $(this).html().replace(/\$6\.77/g,'₹₲2') );
+	$(this).html( $(this).html().replace(/\$15\.31/g,'₹₲4') );
+	$(this).html( $(this).html().replace(/\$9\.10/g,'₹₲3') );
+	$(this).html( $(this).html().replace(/\$15\.33/g,'₹₲4') );
+	$(this).html( $(this).html().replace(/\$15\.15/g,'₹₲4') );
+	$(this).html( $(this).html().replace(/\$20\.00/g,'₹₲5') );
+	$(this).html( $(this).html().replace(/\$17\.04/g,'₹₲4') );
+	$(this).html( $(this).html().replace(/\$50\.00/g,'₹₲13') );
+	$(this).html( $(this).html().replace(/\$16\.99/g,'₹₲4') );
+	$(this).html( $(this).html().replace(/\$3\.99/g,'₹₲1') );
+	$(this).html( $(this).html().replace(/\$21\.46/g,'₹₲5') );
+	$(this).html( $(this).html().replace(/\$133\.45/g,'₹₲33') );
+	$(this).html( $(this).html().replace(/\$30\.26/g,'₹₲8') );
+	$(this).html( $(this).html().replace(/\$9\.95/g,'₹₲2') );
+	$(this).html( $(this).html().replace(/\$26\.12/g,'₹₲7') );
+	$(this).html( $(this).html().replace(/\$12\.49/g,'₹₲3') );
+		$(this).html( $(this).html().replace(/\$17\.98/g,'₹₲5') );
+		});
---
+++ pluginmain/JS/odometer.js
@@ -0,0 +1,1 @@
+odoo.default({ el:'.js-odoo', from: 'NOVEMBER16', to: 'CODEVEMBER', animationDelay: 1000 });
